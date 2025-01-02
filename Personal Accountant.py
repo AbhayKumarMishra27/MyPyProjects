accfa6921a5d85c7a76056b627ea23903712792a
AbhayKumarMishra27 <abhay01mishra36@gmail.com>
2025-01-02 11:04:22 +0530

    Personal Accountant.py

--- Personal Accountant.py
+++ Personal Accountant.py
@@ -26,7 +26,7 @@ def delete_all_expenses():
             with open(EXPENSES_FILE, mode='w', newline='') as file:
                 fieldnames = ['date', 'amount', 'category', 'description']
                 writer = csv.DictWriter(file, fieldnames=fieldnames)
-                writer.writeheader()  # Overwrite the file with just the header (no data)
+                writer.writeheader()
             print("All expenses have been deleted.\n")
         except Exception as e:
             print(f"Error deleting expenses: {e}")
@@ -35,7 +35,6 @@ def delete_all_expenses():
 
 def track_expense():
     print("\n--- Track New Expense ---")
-    
     amount = float(input("Enter amount: ₹"))
     category = input("Enter category (e.g., food, entertainment, transport,other): ").lower()
     description = input("Enter description: ")
@@ -45,11 +44,9 @@ def track_expense():
 
 def display_expenses():
     expenses = read_expenses()
-    
     if not expenses:
         print("\nNo expenses recorded yet.\n")
         return
-    
     print("\n--- All Expenses ---")
     for expense in expenses:
         print(f"{expense['date']} - ₹{expense['amount']} - {expense['category']} - {expense['description']}")
@@ -57,7 +54,6 @@ def display_expenses():
 
 def display_summary():
     expenses = read_expenses()
-    
     if not expenses:
         print("\nNo expenses recorded yet.\n")
         return
@@ -75,12 +71,11 @@ def display_summary():
     
     print("\n--- Expense Summary ---")
     for category, total in summary.items():
-        print(f"{category.capitalize()}: ${total:.2f}")
+        print(f"{category.capitalize()}: ₹{total:.2f}")
     print()
 
 def main():
     print("Welcome I am your Personal Accountant\n")
-    
     while True:
         print("Options:")
         print("1. Track new expense")
